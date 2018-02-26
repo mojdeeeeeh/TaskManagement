307b6e7422723dff10f5cef13afd3eed8831af57
AEPP <aepp@Linux.GNet.Net>
2018-02-26 23:36:17 +0330

    Add Vue,Axios modules into app.js

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-// Route::view('/test', 'test');
-// Route::view('/test-vue', 'test_vue');
-// Route::view('/test2-vue', 'test2-vue');
+Route::view('/','welcome');
 
-// Route::get('/test/data', function(){
-// 	$users = \App\User::all();
+Route::get('/home', 'HomeController@index')->name('home');
 
-// 	return $users;
-// });
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books/data', 'BookController@bookData');
+Route::resource('/books', 'BookController');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/books/data', 'BookController@bookData');
-Route::resource('/books', 'BookController');
